dc4f1d8eede8bd0cf59b0b9bb7d1c1ac58289b8c
flaviusburca <flavius.burca@gmail.com>
2025-01-22 17:10:48 +0200

    - move to github pages

--- upload-s3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-npm run build
-
-aws s3 rm s3://docs.statemesh.net --recursive
-aws s3 cp build/ s3://docs.statemesh.net --recursive
-aws cloudfront create-invalidation --distribution-id E20AKDRD0MU3GU --paths "/*"
-exit
